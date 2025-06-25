f682174acd532493d7868d61fb47f6cfca030a49
Renda Zhang <rendazhang@qq.com>
2025-06-25 21:47:41 +0800

    Add vpc id and subnet ids output

--- infra/aws/outputs.tf
+++ infra/aws/outputs.tf
@@ -1,3 +1,16 @@
 output "alb_dns" {
   value = module.alb.alb_dns
 }
+
+output "vpc_id" {
+  description = "VPC ID created by network_base"
+  value       = module.network_base.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.network_base.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.network_base.private_subnet_ids
+}
